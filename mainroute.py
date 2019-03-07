7d909fa157cc1521cfa65c20c6e33fc29d9ee3fb
ijsimo <31279371+ijsimo@users.noreply.github.com>
2019-03-07 13:44:27 +0000

    Add files via upload

--- mainroute.py
+++ mainroute.py
@@ -1,31 +1,37 @@
 import csv
 
+class Runner:
+    def __init__(self, fname, sname, s):
+        self.fname = fname
+        self.sname = sname
+        self.distance = float(s)
+
 def readData(filename):
     members = []
     f = open(filename, 'r')
-
     with f:
         reader = csv.reader(f)
-
         for row in reader:
-            members.append((row[0], row[1], row[2]))
+            members.append(Runner(row[0], row[1], float(row[2])))
     f.close()
     return members
 
 def FindMax(m):
     ##initialise max with first value
-    maxVal = float(m[0][2])
-    ##loop through each entry in 
+    maxVal = None
+    ##loop through each entry in members
     for d in m:
-        if float(d[2]) > maxVal:
-            maxVal = float(d[2])
+        if maxVal == None:
+            maxVal = d.distance
+        elif d.distance > maxVal:
+            maxVal = d.distance
     return maxVal
 
 def Awards(m, l):
     ##m is the list of performances, l is the performance limit
     a = []
     for p in m:
-        if float(p[2]) > l:
+        if p.distance > l:
             a.append(p)
     return a
 
@@ -37,7 +43,8 @@ def Main():
     print('Max Value: ' + str(maxVal))
     ##find everyone with a score at least 70% of the maximum
     awards = Awards(members, 0.7 * maxVal)
-    print(awards)
+    for a in awards:
+        print(a.fname + " " + a.sname + " " + str(a.distance))
 
 Main()
                 
